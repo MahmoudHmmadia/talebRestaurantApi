4fe2de6febf103038b0ed6d9057bd3859726876f
Mahmoud Hmmadia <102804429+MahmoudHmmadia@users.noreply.github.com>
2023-06-28 17:17:22 +0300

    fix cors

--- routes/root/index.ts
+++ routes/root/index.ts
@@ -1,7 +1,10 @@
-import path from "path";
+import { fileURLToPath } from "url";
+import path, { dirname } from "path";
 import express, { Request, Response } from "express";
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 const router = express.Router();
 router.get("^/$|/index(.html)?", (_req: Request, res: Response) => {
-  res.sendFile(path.join(__dirname, "..", "views", "index.html"));
+  res.sendFile(path.join(__dirname, "..", "..", "views", "index.html"));
 });
 export default router;
